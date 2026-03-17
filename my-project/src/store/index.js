c06d7d67cab7b29fb2a70164eafbb9277fdcb12c
deniz yildiz <yildizdeniztaylan@gmail.com>
2026-03-17 12:45:26 +0300

    T09: Done
    Initialize Redux

--- my-project/src/store/index.js
+++ my-project/src/store/index.js
@@ -1,8 +1,19 @@
-import { configureStore } from '@reduxjs/toolkit';
-// Buraya daha sonra oluşturacağın slice'ları (reducer) import edeceksin
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { thunk } from 'redux-thunk';
+import logger from 'redux-logger';
 
-export const store = configureStore({
-  reducer: {
-   app: (state = {}) => state
-  },
+// Reducer'larını import et (Eğer oluşturduysan)
+import { clientReducer } from './reducers/clientReducer';
+import { productReducer } from './reducers/productReducer';
+import { shoppingCartReducer } from './reducers/shoppingCartReducer';
+
+const rootReducer = combineReducers({
+  client: clientReducer,
+  product: productReducer,
+  shoppingCart: shoppingCartReducer
 });
+
+export const store = createStore(
+  rootReducer, 
+  applyMiddleware(thunk, logger)
+);
